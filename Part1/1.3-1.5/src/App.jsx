433d7eae2283099adc6fb359dacc56f677c7135a
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-02 19:09:08 +0545

    wrap everything into a single course object that contains both the course name and the array of parts

--- Part1/1.3-1.5/src/App.jsx
+++ Part1/1.3-1.5/src/App.jsx
@@ -3,18 +3,28 @@ import Content from './Components/Content'
 import Total from './Components/Total'
 
 const App = () => {
-  const course = 'Half Stack application development'
-  /*const part1 = { name: 'Fundamentals of React', exercises: 10 }
+  /*const course = 'Half Stack application development'
+  const part1 = { name: 'Fundamentals of React', exercises: 10 }
   const part2 = { name: 'Using props to pass data', exercises: 7 }
   const part3 = { name: 'State of a component', exercises: 14 }
   */
 
   // Putting all part into array instead of separate objects
- const parts = [
+ /*const parts = [
     { name: 'Fundamentals of React', exercises: 10 },
     { name: 'Using props to pass data', exercises: 7 },
     { name: 'State of a component', exercises: 14 }
   ]
+    */
+  // 
+   const course = {
+    name: 'Half Stack application development',
+    parts: [
+      { name: 'Fundamentals of React', exercises: 10 },
+      { name: 'Using props to pass data', exercises: 7 },
+      { name: 'State of a component', exercises: 14 }
+    ]
+  }
 
   return (
     /*<div>
@@ -24,10 +34,17 @@ const App = () => {
     </div>
     */
 
-     <div>
+    /* <div>
       <Header course={course} />
       <Content parts={parts} />
       <Total parts={parts} />
+    </div>
+    */
+
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
--- Part1/1.3-1.5/src/Components/Total.jsx
+++ Part1/1.3-1.5/src/Components/Total.jsx
@@ -1,10 +1,16 @@
 import React from "react"
-const Total = ({ parts }) => {
+/*const Total = ({ parts }) => {
   return (
     <p>
       Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   )
+}
+  */
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return <p>Number of exercises {total}</p>
 }
 
 export default Total
