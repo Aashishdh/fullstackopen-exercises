fb0263782ed7aa3c0cd2732fb4b8629d2d4431b4
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-02 19:02:25 +0545

    Create single array instead of separate part

--- Part1/1.3-1.5/src/Components/Content.jsx
+++ Part1/1.3-1.5/src/Components/Content.jsx
@@ -1,10 +1,20 @@
 import Part from './Part'
-
-const Content = ({ part1, part2, part3 }) => (
+// Repalce all parts with Parts
+/* const Content = ({ part1, part2, part3 }) => (
   <div>
     <Part name={part1.name} exercises={part1.exercises} />
     <Part name={part2.name} exercises={part2.exercises} />
     <Part name={part3.name} exercises={part3.exercises} />
+  </div>
+  
+
+)*/
+
+const Content = ({ parts }) => (
+  <div>
+    <Part name={parts[0].name} exercises={parts[0].exercises} />
+    <Part name={parts[1].name} exercises={parts[1].exercises} />
+    <Part name={parts[2].name} exercises={parts[2].exercises} />
   </div>
 )
 
--- Part1/1.3-1.5/src/Components/Total.jsx
+++ Part1/1.3-1.5/src/Components/Total.jsx
@@ -1,5 +1,12 @@
-const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>
+import React from "react"
+const Total = ({ parts }) => {
+  return (
+    <p>
+      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  )
 }
 
 export default Total
+
+
