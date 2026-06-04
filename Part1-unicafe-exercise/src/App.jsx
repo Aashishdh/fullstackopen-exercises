355885ad6b7e8b387cb068dd6c7dc7a7bc574738
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-04 19:38:24 +0545

    compeleted 1.7 using ternary operator and find all, average and percentage

--- Part1-unicafe-exercise/src/App.jsx
+++ Part1-unicafe-exercise/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const all= good+neutral+bad;
   return (
     <div>
       <div className='m-6'>
@@ -34,6 +35,9 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral} </p>
         <p>bad: {bad} </p>
+        <p>all: {all}</p>
+         <p>average: {all > 0 ? (good * 1 + neutral * 0 + bad * -1) / all : 0}</p>
+        <p>Positive: {all>0 ? (good / all) * 100:0}%</p>
       </div>
     </div>
   )
