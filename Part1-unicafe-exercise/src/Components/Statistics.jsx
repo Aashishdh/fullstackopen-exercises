7892fe983ee95114702357d4bd9ae8828fc4dbfd
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-06 15:20:11 +0545

    statistics are only displayed if feedback has already been

--- Part1-unicafe-exercise/src/Components/Statistics.jsx
+++ Part1-unicafe-exercise/src/Components/Statistics.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 const Statistics = ({ good, neutral, bad }) => {
     const all= good+neutral+bad;
+    // statistics are only displayed if feedback has already been given
+    if(all===0){
+      return <p>No feedback given</p>
+    }
     const average=all > 0 ? (good * 1 + neutral * 0 + bad * -1) / all : 0
     const Positive= all>0 ? (good / all) * 100:0
   return (
