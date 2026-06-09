28a0ed2f273a3ca44ec28cf81f4d852c8b609bb4
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-09 18:13:13 +0545

    Understand some knowledge about reduce and completed 2.3

--- Part2-course-information/src/Components/Total.jsx
+++ Part2-course-information/src/Components/Total.jsx
@@ -1,6 +1,9 @@
 import React from "react"
 const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+ const total = parts.reduce((sum, part) => {
+  console.log('what is happening', sum, part)
+  return sum + part.exercises
+}, 0)
   return <p><strong>total of {total} exercises</strong></p>
 }
 
