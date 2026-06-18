4ea324184fc7ae23b09f35c69bfbcd3050dcf47c
Aashish Dhungana <ashisdhungana2062@gmail.com>
2026-06-18 06:48:13 +0545

    2.12 is completed

---
+++ Part2-Editing_data/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Filter from './Components/Filter'
+import PersonForm from './Components/PersonForm'
+import Persons from './Components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (e) => {
+    e.preventDefault()
+    if (persons.some(elem => elem.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const nameObject = { name: newName, number: newNumber }
+    axios.post('http://localhost:3001/persons', nameObject)
+    .then(response => {
+      setPersons(persons.concat(response.data))
+      setNewName('')
+      setNewNumber('')
+    })
+  }
+
+  const personsToShow = persons.filter(elem =>
+    elem.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleFilterChange={(e) => setFilter(e.target.value)} />
+      <h3>Add a new</h3>
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={(e) => setNewName(e.target.value)}
+        newNumber={newNumber}
+        handleNumberChange={(e) => setNewNumber(e.target.value)}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
